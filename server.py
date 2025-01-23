560a49d06a082017f3d16370ece9d9e9e60b8df5
MatteoVerini <mattverini@gmail.com>
2025-01-23 10:41:58 +0100

    classe MenuItem e funzioni menu

--- server.py
+++ server.py
@@ -3,3 +3,35 @@ import os, json
 
 template_dir = os.path.abspath("./templates")
 app = Flask(__name__, template_folder=template_dir)
+
+class MenuItem:
+    def __init__(self, id, name, description, price):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.price = price
+
+    def GetID(self):
+        return self.id
+    
+    def GetName(self):
+        return self.name
+
+    def GetDescription(self):
+        return self.description
+    
+    def GetPrice(self):
+        return self.price
+    
+menu_file = "db/menu.json"
+
+def load_menu():
+    with open(menu_file, 'r') as file:
+        return json.load(file)
+
+def save_menu(menu_items):
+    with open(menu_file, 'w') as file:
+        json.dump(menu_items, file, indent=4)
+
+        
+
