7c049fec96b8e3f7bfdab43840126a5130a30b54
MatteoVerini <mattverini@gmail.com>
2025-01-23 18:25:08 +0100

    Python metodi

--- server.py
+++ server.py
@@ -54,23 +54,36 @@ def add_menu_item():
     save_menu(menu_items)
     return redirect('/')
 
-    @app.route('/edit/<int:item_id>', methods=["GET"])
-    def edit_menu_page(item_id):
-        menu_items = load_menu()
-        item_to_edit = next((item for item in menu_items if item["id"] == item_id), None)
+#Editing an element
+@app.route('/edit/<int:item_id>', methods=["GET"])
+def edit_menu_page(item_id):
+    menu_items = load_menu()
+    item_to_edit = next((item for item in menu_items if item["id"] == item_id), None)
     if not item_to_edit:
-        return "Elemento non trovato", 404
+        return "Element Not Found", 404
     return render_template("edit_menu.html", item=item_to_edit)
 
-
-    @app.route('/update/<int:item_id>', methods=["POST"])
-    def update_menu_item(item_id):
-        menu_items = load_menu()
-        for item in menu_items:
-            if item["id"] == item_id:
-                item["name"] = request.form["name"]
-                item["description"] = request.form["description"]
-                item["price"] = float(request.form["price"])
+#Visualizing the edit page
+@app.route('/update/<int:item_id>', methods=["POST"])
+def update_menu_item(item_id):
+    menu_items = load_menu()
+    for item in menu_items:
+         if item["id"] == item_id:
+            item["name"] = request.form["name"]
+            item["description"] = request.form["description"]
+            item["price"] = float(request.form["price"])
             break
-        save_menu(menu_items)
-        return redirect('/')
+    save_menu(menu_items)
+    return redirect('/')
+
+#Deleting an item
+@app.route('/delete/<int:item_id>', methods=["POST"])
+def delete_menu_item(item_id):
+    menu_items = load_menu()
+    menu_items = [item for item in menu_items if item["id"] != item_id]
+    save_menu(menu_items)
+    return redirect('/')
+
+#Flask debug
+if __name__ == '__main__':
+    app.run(debug=True)
